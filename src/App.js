7f65851d827f2489f430c6c76668301ba0d96b48
Hussain Ahmed Siddiqui <64015627+Hussain101@users.noreply.github.com>
2023-01-09 04:51:19 -0800

    Update App.js
    commited

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App bg-[#F5F5F5] ">
-      <Routes>
-        <Route exact path="/" element={<Homepage />} />
-      </Routes>
+     <p> hello </p>
     </div>
   );
 }
